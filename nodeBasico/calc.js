96b3b4d7a6d0ac7e701cafac3260a4923ad2f933
Alex Junior Barreto <alexjuniorbarreto@gmail.com>
2023-04-10 12:08:46 -0300

    curso node

---
+++ nodeBasico/calc.js
@@ -0,0 +1,9 @@
+var SomaFunc = require("./somar");
+var SubFunc = require("./sub");
+var MultFunc = require("./mult");
+var DivFunc = require("./div");
+
+console.log(SomaFunc(1, 2));
+console.log(SubFunc(10, 5));
+console.log(MultFunc(100, 10));
+console.log(DivFunc(10, 1));

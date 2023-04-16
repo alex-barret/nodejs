136c3d46ca576cf25830e459a07d3a5f649c5257
Alex Junior Barreto <alexjuniorbarreto@gmail.com>
2023-04-16 00:36:10 -0300

    1

--- teste.js
+++ teste.js
@@ -1,1 +1,11 @@
 const Sequelize = require('sequelize')
+const sequelize = new Sequelize('teste2', 'root', 'Lancer@2022', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+sequelize.authenticate().then(function () {
+    console.log("conectado com sucesso")
+}).catch(function (error) {
+    console.log("falha ao se conectar: " + error)
+})

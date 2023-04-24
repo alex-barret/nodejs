98f51cbe085684d75bb6ba2a27549878e1628633
Alex Junior Barreto <alexjuniorbarreto@gmail.com>
2023-04-24 11:51:35 -0300

    a

--- teste2/teste2.js
+++ teste2/teste2.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('teste2', 'root', 'Lancer@2022', {
+const sequelize = new Sequelize('teste', 'root', 'Lancer@2022', {
     host: 'localhost',
     dialect: 'mysql'
 })

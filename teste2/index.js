2260cde03cdcda9b3abd298679cb342ceb0a5327
Alex Junior Barreto <alexjuniorbarreto@gmail.com>
2023-04-27 12:24:14 -0300

    h

--- teste2/index.js
+++ teste2/index.js
@@ -3,20 +3,21 @@ const app = express();
 const handlebars = require('express-handlebars')
 const Sequelize = require('sequelize')
 
-
-
 // config
 //tamplate engine
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
-
+app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 //conexão com o banco de dados mysql
 const sequelize = new Sequelize('teste', 'root', 'Lancer@2022', {
     host: 'localhost',
     dialect: 'mysql'
 })
+//Rotas
+app.get('/cad', function (req, res) {
+    res.render('formulario')
+})
 
 app.listen(8081, function () {
     console.log("servidor rodando na url http://localhost:8081");
-});
+}); 
